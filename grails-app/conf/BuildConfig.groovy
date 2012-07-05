9512a57e4f2574191a9e241a17ca57f2b81342fc
Eyck Jentzsch <eyck@jepemuc.de>
2012-07-05 18:47:39 +0200

    Upgraded to BIRT version 4.2.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,7 +2,6 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
-
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -12,5 +11,5 @@ grails.project.dependency.resolution = {
     log "warn"
     repositories {
 		grailsRepo "http://grails.org/plugins"
-	}
+    }
 }
--- lib/README.txt
+++
@@ -1,1 +0,0 @@
-This is a placeholder. To build the plugin the jars of the BIRT runtime need to be copied here.
