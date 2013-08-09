a85db41e8f424c22be7750e9a5f070c57eea9f71
Burt Beckwith <burt@burtbeckwith.com>
2013-08-09 14:09:09 -0400

    added missing properties

--- BirtReportGrailsPlugin.groovy
+++ BirtReportGrailsPlugin.groovy
@@ -1,8 +1,18 @@
 class BirtReportGrailsPlugin {
-    def version = "4.3.0.0"
-    def author = "Eyck Jentzsch"
-    def authorEmail = "eyck@jepemuc.de"
-    def title = "Birt Reporting Plugin"
-    def description = 'Makes it easy to integrate the Birt reporting engine (runtime component) into your Grails application. Currently BIRT 4.3.0 is used.'
-    def documentation = "http://grails.org/plugin/birt-report"
+	def version = "4.3.0.0"
+	def grailsVersion = "2.0 > *"
+	def author = "Eyck Jentzsch"
+	def authorEmail = "eyck@jepemuc.de"
+	def title = "Birt Reporting Plugin"
+	def description = 'Makes it easy to integrate the Birt reporting engine (runtime component) into your Grails application. Currently BIRT 4.3.0 is used.'
+	def documentation = "http://grails.org/plugin/birt-report"
+
+	def license = 'APACHE'
+	def issueManagement = [system: 'JIRA', url: 'https://github.com/eyck/grails-birt-report/issues']
+	def scm = [url: 'https://github.com/eyck/grails-birt-report']
+
+	def pluginExcludes = [
+		'lib/**',
+		'web-app/**'
+	]
 }
